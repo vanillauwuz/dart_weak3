bee509b7085395bf40638c6d07e871d2e77fa05a
vanilla <kodyiwn@gmail.com>
2023-05-30 13:31:48 -0800

    update

---
+++ bin/commit101.dart
@@ -0,0 +1,5 @@
+void main(){
+ const String name = "Hello world!";
+ //name \ "Elon mask";
+ print(name);
+} 
---
+++ bin/final101.dart
@@ -0,0 +1,5 @@
+void main(){
+  final String name;
+  name = "Elon mask";
+  print(name)
+}
