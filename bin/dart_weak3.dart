4cf107569a6c8160d76833959b2b8a1d68094ec2
vanilla <kodyiwn@gmail.com>
2023-05-30 12:30:21 -0800

    weak3

---
+++ bin/dart_weak3.dart
@@ -0,0 +1,5 @@
+import 'package:dart_weak3/dart_weak3.dart' as dart_weak3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_weak3.calculate()}!');
+}
---
+++ test/dart_weak2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_weak2/dart_weak2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
